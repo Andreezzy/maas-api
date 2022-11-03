c39b4d593e721639202aaf06210da72f10184988
Andreezzy <frandres333@gmail.com>
2022-11-03 15:23:11 -0500

    add validations on User model to pass the tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates :email, presence: true, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :name, presence: true
+  validates :last_name, presence: true
+  validates :password,
+            length: { minimum: 8 }
 end
