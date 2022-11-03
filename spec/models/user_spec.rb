e613ac5b8895984be6b8f30d7b401418f70151f5
Andreezzy <frandres333@gmail.com>
2022-11-03 15:22:46 -0500

    fix method names of shoulda-matchers

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it { should validates_presence_of(:name) }
-  it { should validates_presence_of(:last_name) }
-  it { should validates_presence_of(:email) }
-  it { should validates_presence_of(:password) }
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:last_name) }
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:password) }
 
-  it { should validates_uniqueness_of(:email) }
-  it { should validates_length_of(:password).is_at_least(8) }
+  it { should validate_uniqueness_of(:email) }
+  it { should validate_length_of(:password).is_at_least(8) }
 end
