6d4a88ec9bf6ab28845244248268be3bafc7722d
Andreezzy <frandres333@gmail.com>
2022-11-03 15:05:04 -0500

    add rspec tests for User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validates_presence_of(:name) }
+  it { should validates_presence_of(:last_name) }
+  it { should validates_presence_of(:email) }
+  it { should validates_presence_of(:password) }
+
+  it { should validates_uniqueness_of(:email) }
+  it { should validates_length_of(:password).is_at_least(8) }
 end
