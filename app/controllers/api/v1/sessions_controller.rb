c1e53dab1bfbe17d04f102b590f8cc84d20358fc
Andreezzy <frandres333@gmail.com>
2022-11-03 20:04:09 -0500

    create sessions controller

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,22 @@
+class Api::V1::SessionsController < Api::V1::ApiController
+  skip_before_action :authenticate_user, only: :create
+
+  # POST /sessions
+  def create
+    @user = User.find_by_email(params[:email])
+    if @user&.authenticate(params[:password])
+      token = JsonWebToken.encode(user_id: @user.id)
+      time = Time.now + 24.hours.to_i
+      render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"),
+                     email: @user.email }, status: :ok
+    else
+      render json: { error: 'unauthorized' }, status: :unauthorized
+    end
+  end
+
+  private
+
+  def login_params
+    params.permit(:email, :password)
+  end
+end
