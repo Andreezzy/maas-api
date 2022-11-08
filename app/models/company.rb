2df536739adb4d6c254263ffbf3634f9af89fd89
Andreezzy <frandres333@gmail.com>
2022-11-08 10:44:48 -0500

    add has many schedules to company model

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,6 @@
 class Company < ApplicationRecord
+  has_many :schedules
+
   validates :name, presence: true
   validates :avatar, presence: true
   validates :description, presence: true
--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Company, type: :model do
+  it { should have_many(:schedules) }
+
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:avatar) }
   it { should validate_presence_of(:description) }
