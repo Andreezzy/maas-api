9e9d80b65303685538331a1c6bce82cae223a10d
Andreezzy <frandres333@gmail.com>
2022-11-08 01:43:05 -0500

    add validates_inclusion_of to BusinessHour day_of_week field

--- app/models/business_hour.rb
+++ app/models/business_hour.rb
@@ -1,7 +1,9 @@
 class BusinessHour < ApplicationRecord
   belongs_to :schedule
-
+  
   validates :day_of_week, presence: true
   validates :start_time, presence: true
   validates :end_time, presence: true
+
+  validates_inclusion_of :day_of_week, :in => 0..6
 end
--- spec/models/business_hour_spec.rb
+++ spec/models/business_hour_spec.rb
@@ -6,4 +6,6 @@ RSpec.describe BusinessHour, type: :model do
   it { should validate_presence_of(:day_of_week) }
   it { should validate_presence_of(:start_time) }
   it { should validate_presence_of(:end_time) }
+
+  it { should validate_inclusion_of(:day_of_week).in_range(0..6) }
 end
