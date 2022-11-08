8e3488cfd69b0fc123d57069ecad10f5b8b7df16
Andreezzy <frandres333@gmail.com>
2022-11-08 10:29:45 -0500

    fix user factory to pass the tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,6 +3,8 @@ FactoryBot.define do
     name { 'Ash' }
     last_name { 'Perez' }
     email { 'ash@gmail.com' }
+    color { '#FF8787' }
+    avatar { 'https://github.com/Andreezzy/profile-pictures/blob/main/avatar-1.png' }
     password { 'password'}
     password_confirmation { 'password' }
   end
