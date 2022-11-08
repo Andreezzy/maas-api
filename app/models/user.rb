ec1fccf1f3c0bb7bc5559c28a9aaefd380317b9a
Andreezzy <frandres333@gmail.com>
2022-11-08 00:35:50 -0500

    update user validations with its tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :name, presence: true
   validates :last_name, presence: true
+  validates :avatar, presence: true
+  validates :color, presence: true, length: { is: 7 }
   validates :password, length: { minimum: 8 }, if: -> { new_record? }
   validates :password_confirmation, presence: true, if: -> { new_record? }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,9 +4,12 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:last_name) }
   it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:avatar) }
+  it { should validate_presence_of(:color) }
   it { should validate_presence_of(:password) }
   it { should validate_presence_of(:password_confirmation) }
 
   it { should validate_uniqueness_of(:email) }
   it { should validate_length_of(:password).is_at_least(8) }
+  it { should validate_length_of(:color).is_equal_to(7) }
 end
