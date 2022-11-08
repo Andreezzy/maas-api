306c92693e800ead73b6319ef39e33fb219c7f67
Andreezzy <frandres333@gmail.com>
2022-11-08 11:01:31 -0500

    add tests and validations for Event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,10 @@
 class Event < ApplicationRecord
   belongs_to :schedule
-  belongs_to :user
+  belongs_to :user, optional: true
+
+  validates :kind, presence: true
+  validates :start_time, presence: true
+  validates :end_time, presence: true
+
+  enum :kind, { draft: 0, published: 1 }
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:schedule) }
+  it { should belong_to(:user).optional }
+
+  it { should validate_presence_of(:kind) }
+  it { should validate_presence_of(:start_time) }
+  it { should validate_presence_of(:end_time) }
+
+  it { should define_enum_for(:kind) }
+  it { should define_enum_for(:kind).with_values(draft: 0, published: 1) }
+
 end
